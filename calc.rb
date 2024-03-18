f86c68dae025ed865a9ac17810fa72bfca779fc1
Juan .C <160873765+reddevx@users.noreply.github.com>
2024-03-18 00:50:30 +0000

    Create calc.rb

---
+++ calc.rb
@@ -0,0 +1,19 @@
+print "Enter the first number: "
+number1 = gets.chomp.to_i
+
+puts = "Ok, registered number."
+
+print "Enter the second number: "
+number2 = gets.chomp.to_i
+
+puts = "Ok, registered number."
+
+addition = number1 + number2
+subtraction = number1 - number2
+division = number1 / number2
+multiplication = number1 * number2
+
+puts "Addition: The result of the sum is #{addition}"
+puts "Subtraction: The result of the sum is #{subtraction}"
+puts "Division: The result of the sum is #{division}"
+puts "Multiplication: The result of the sum is #{multiplication}"
